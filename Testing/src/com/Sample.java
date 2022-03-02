9020f339726d125a7521be8942bab50dd4013be2
wa444777 <64914988+wa444777@users.noreply.github.com>
2022-03-02 20:53:27 +0530

    First Commit

---
+++ Testing/src/com/Sample.java
@@ -0,0 +1,15 @@
+package com;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=a+b;
+		int d=c*30;
+		
+		System.out.println("Result : "+d);
+		
+	}
+
+}
